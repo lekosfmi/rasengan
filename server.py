62616031719021f88f13336dd5d0bfb9929d3576
Vanielle Le <lephanvanielle@gmail.com>
2017-01-20 20:50:52 -0800

    modify server

--- server.py
+++ server.py
@@ -6,5 +6,7 @@ app = Flask(__name__, static_url_path = '/static')
 def root():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
